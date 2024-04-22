4abd54ec3b1ce5b277d530cdd2e2bb2b2e7faea3
Vitalii Matvieiev <vitalik.matveev133@gmail.com>
2024-04-21 19:01:19 -0500

    init

---
+++ CacheStampedeResolver/CacheStampedeResolver/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Diagnostics;
+using StackExchange.Redis;
+
+public class Cache
+{
+    private ConnectionMultiplexer _redis;
+    private IDatabase _db;
+    public Cache()
+    {
+        _redis = ConnectionMultiplexer. Connect("localhost:6379, abortConnect=false");
+        _db = _redis.GetDatabase();
+    }
+        
+    public object XFetch(string key, double beta = 1)
+    {
+        
+        var chaceRedisValue = _db.StringGetWithExpiry(key);
+        if (chaceRedisValue.Value.HasValue && chaceRedisValue.Expiry.HasValue)
+        {
+            var delta = DateTime.Now.Second - chaceRedisValue.Expiry.Value.TotalSeconds;
+            if (chaceRedisValue.Value.IsNull ||
+                DateTime.Now.Second - (int)delta * beta * Math.Log(new Random().NextDouble()) >=
+                chaceRedisValue.Expiry.Value.TotalSeconds) ;
+            {
+                var value = RecomputeValue();
+                _db.StringSet(key, value);
+            }
+            return chaceRedisValue.Value;
+        }
+        else
+        {
+            var value = RecomputeValue();
+            _db.StringSet(key, value);
+            return value
+        }
+    }
+    
+    private string RecomputeValue()
+    {
+        // For demonstration purposes, let's return a simple string
+        return "Recomputed Value";
+    }
+}
